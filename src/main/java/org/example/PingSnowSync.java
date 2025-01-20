576fb2f868525a498b4c9540ae0fbfef673c2f84
srallapally <sanjay.rallapally@gmail.com>
2025-01-19 17:14:49 -0800

    Removed dead code from PingSnowSync

--- src/main/java/org/example/PingSnowSync.java
+++ src/main/java/org/example/PingSnowSync.java
@@ -1,20 +1,5 @@
 package org.example;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.*;
-import org.apache.http.HttpEntity;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
 import org.example.auth.PingAuthenticator;
 import org.example.auth.SnowAuthenticator;
 import org.example.client.SnowClient;
@@ -22,15 +7,10 @@ import org.example.sync.EntitlementSyncHandler;
 import org.example.sync.RoleSyncHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStream;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 import static java.lang.System.exit;
 
@@ -133,211 +113,8 @@ public class PingSnowSync {
             return config;
         }
     }
-   /*
-    public static void syncAppCatalogItems(Properties config) throws Exception {
-        // syncItems("app", queries.getProperty("app_query"),
-        //         queries.getProperty("app_payload"), appTemplatePath);
-        // syncItems("role", queries.getProperty("role_query"),
-        //         queries.getProperty("role_payload"), roleTemplatePath);
-        //syncItems("entitlement",
-        //        config.getProperty("ping.tenant.url")+ config.getProperty("ping.entitlement.query"),
-        //        config.getProperty("ping.entitlement.query.body"));
-        throw new UnsupportedOperationException();
-    }
-    public static void syncRoleCatalogItems(Properties config) throws Exception {
-        // syncItems("app", queries.getProperty("app_query"),
-        //         queries.getProperty("app_payload"), appTemplatePath);
-        // syncItems("role", queries.getProperty("role_query"),
-        //         queries.getProperty("role_payload"), roleTemplatePath);
-        //syncItems("entitlement",
-        //        config.getProperty("ping.tenant.url")+ config.getProperty("ping.entitlement.query"),
-        //        config.getProperty("ping.entitlement.query.body"));
-        throw new UnsupportedOperationException();
-    }
-    */
-    /*
-    public static void syncEntitlementCatalogItems(Properties config) throws Exception {
-       // syncItems("app", queries.getProperty("app_query"),
-       //         queries.getProperty("app_payload"), appTemplatePath);
-       // syncItems("role", queries.getProperty("role_query"),
-       //         queries.getProperty("role_payload"), roleTemplatePath);
-        syncItems("entitlement",
-                config.getProperty("ping.tenant.url")+ config.getProperty("ping.entitlement.query"),
-                config.getProperty("ping.entitlement.query.body"));
-    }
-
-     */
-/*
-    private static void syncItems(String type, String queryTemplate, String queryBody) throws Exception {
-        int offset = 0;
-        boolean hasMore = true;
-        ArrayList<Map<String, Object>> entitlements = new ArrayList<>();
-        logger.debug("queryTemplate: {}", queryTemplate);
-        logger.debug("queryBody: {}", queryBody);
-        Properties mappingProperties = new Properties();
-        String mappingJson = config.getProperty("snow.entitlement.catalog.mapping");
-        logger.debug("mappingJson: {}", mappingJson);
-        mappingProperties = loadConfig(new File(mappingJson));
-        while (hasMore) {
-            String query = queryTemplate.replace("%offset%", String.valueOf(offset));
-            logger.debug("query: {}", query);
-            // Get items from Ping
-            String pingResponse = executePingQuery(query, queryBody);
-            //System.out.println("PingResponse: " + pingResponse);
-            JsonNode pingItems = mapper.readTree(pingResponse);
-            JsonNode results = pingItems.path("result");
-            String totalCount = null;
-            totalCount = pingItems.path("totalCount").asText();
-            logger.debug("totalCount: {}", totalCount);
-            if(totalCount == null) {
-                totalCount = "0";
-            }
-            if (pingItems.isEmpty() || offset >= Integer.parseInt(totalCount) ) {
-                hasMore = false;
-                continue;
-            }
-            // Process each item
-            for (JsonNode item : results) {
-                Map<String, Object> entitlementMap = new HashMap<>();
-                // Extract values based on mappings
-                for (String key : mappingProperties.stringPropertyNames()) {
-                    logger.debug("Entitlement key: {}", key);
-                    String jsonPath = mappingProperties.getProperty(key);
-                    logger.debug("Entitlement jsonPath: {}", jsonPath);
-                    Object value = extractValue(item, jsonPath);
-                    entitlementMap.put(key, value);
-                }
-                entitlements.add(entitlementMap);
-            }
-            offset += Integer.parseInt(config.getProperty("ping.catalog.offset"));
-        }
-
-        for (Map<String, Object> entitlement : entitlements) {
-            String snowUrl = config.getProperty("snow.tenanturl")+config.getProperty("snow.entitlement.catalog.query");
-            logger.debug("snowUrl: {}", snowUrl);
-            String catalogAttr = String.valueOf(config.getProperty("snow.entitlement.linkingAttribute"));
-            logger.debug("catalogAttr: {}", catalogAttr);
-            snowUrl = snowUrl.replace("%snow.entitlement.linkingAttribute%",catalogAttr);
-            logger.debug("snowUrl: {}", snowUrl);
-            String catalogVal = (String) entitlement.get(catalogAttr);
-            logger.debug("catalogVal: {}", catalogVal);
-            if(catalogVal != null)
-                snowUrl = snowUrl.replace("%ping.entitlement.catalog_id%",catalogVal);
-                logger.debug("After replacing Snow URL: {}", snowUrl);
-            //System.out.println("Url:  " + snowUrl);
-            String snowResponse = executeSnowQuery(snowUrl);
-            logger.debug("Snow Response: {}", snowResponse);
-            //System.out.println(snowResponse);
-            var snowItems = mapper.readTree(snowResponse).get("result");
-
-            if (snowItems.isEmpty()) {
-                logger.debug("No snow items found");
-                entitlement.put("snow.tenanturl",String.valueOf(config.getProperty("snow.tenanturl")));
-                entitlement.put("snow.entitlement.category",String.valueOf(config.getProperty("snow.entitlement.category")));
-                entitlement.put("snow.catalogId",String.valueOf(config.getProperty("snow.catalogId")));
-                String itemJson = processTemplate(entitlement);
-                logger.debug("ItemJson: {}", itemJson);
-                //System.out.println(itemJson);
-                // Create new item
-                String postResponse = createSnowItem(itemJson);
-                if(TESTMODE){
-                    logger.debug("TESTMODE skipped creation");
-                } else {
-                    logger.debug("Item postResponse: {}", postResponse);
-                }
-            } else {
-                System.out.println("Found " + snowItems.size() + " snow items");
-                // Update existing item
-                //String sysId = snowItems.get(0).get("sys_id").asText();
-                //updateSnowItem(sysId, itemJson);
-            }
-            System.out.println("Sleeping");
-            TimeUnit.SECONDS.toMillis(2);
-        }
-    }
 
 
-    private static Object extractValue(JsonNode node, String jsonPath) {
-        String[] pathParts = jsonPath.split("/");
-        System.out.println(pathParts[0]);
-        JsonNode current = node;
-        for (String part : pathParts) {
-            //System.out.println("Checking for "+part);
-            if (part.isEmpty()) continue;
-
-            current = current.path(part);
-
-            if (current.isMissingNode()) {
-                //System.out.println(part + " is empty");
-                return null; // Path does not exist
-            }
-        }
-
-        if (current.isTextual()) {
-            return current.asText();
-        } else if (current.isNumber()) {
-            return current.numberValue();
-        } else if (current.isBoolean()) {
-            return current.asBoolean();
-        } else if (current.isArray()) {
-            return current.toString(); // Convert arrays to string for simplicity
-        }
-        return current.toString();
-    }
-    private static String executePingQuery(String url, String payload) throws Exception {
-        try (CloseableHttpClient client = HttpClients.createDefault()) {
-            HttpPost request = new HttpPost(url);
-            request.addHeader("Authorization", "Bearer " + pingAccessToken);
-            request.addHeader("Content-Type", "application/json");
-            request.setEntity(new StringEntity(payload));
-
-            return EntityUtils.toString(client.execute(request).getEntity());
-        }
-    }
-    private static String executeSnowQuery(String url) throws Exception {
-        return snowClient.executeGet(url);
-         /*
-         HttpGet request = new HttpGet(url);
-        request.setHeader(HttpHeaders.ACCEPT, "application/json");
-
-        try (CloseableHttpResponse response = snowClient.ex(request)) {
-            return EntityUtils.toString(snowClient.execute(request).getEntity());
-        }
-    }
-    */
-/*
-    private static String createSnowItem(String jsonBody) throws Exception {
-        String snowUrl = config.getProperty("snow.tenanturl")+config.getProperty("snow.catalog.create");
-        logger.debug("Snow URL: {}", snowUrl);
-        logger.debug("Creating Snow Item");
-        logger.debug("jsonBody: {}", jsonBody);
-        if(!TESTMODE) {
-            //System.out.println(snowUrl);
-
-            return snowClient.executePost(snowUrl,jsonBody);
-        } else {
-            logger.debug("Skipping snow item creation due to test mode");
-        }
-        return null;
-    }
-    private static String processTemplate(Map<String, Object> entitlement) throws Exception {
-        String templName = config.getProperty("snow.entitlement.create.template");
-        String templ = null;
-        // Read the file from resources folder
-        try (InputStream inputStream = new FileInputStream(templName)) {
-            // Read the content using UTF-8 encoding
-            templ = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-        }
-        for (Map.Entry<String, Object> entry : entitlement.entrySet()) {
-            templ = templ.replace("%" + entry.getKey() + "%", String.valueOf(entry.getValue()));
-        }
-        // workflow
-        templ = templ.replace("%snow.entitlement.workflow_id%", config.getProperty("snow.entitlement.workflow_id"));
-        //icon and picture
-        templ = templ.replace("%snow.entitlement_icon_id%", config.getProperty("snow.entitlement_icon_id"));
-        return templ;
-    }
-*/
     public static boolean isEmpty(final String val) {
         return val == null || (val.isEmpty());
     }
